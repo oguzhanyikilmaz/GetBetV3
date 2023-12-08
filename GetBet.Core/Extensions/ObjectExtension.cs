a2d03c42f95837d2399e156152f7c4ac3432fa7f
Oğuzhan Yıkılmaz <oguzhan.yikilmaz@rnd.com.tr>
2023-12-08 17:43:56 +0300

    ObjectExtension

---
+++ GetBet.Core/Extensions/ObjectExtension.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetBet.Core.Extensions
+{
+    public static class ObjectExtension
+    {
+        public static string ToJson<T>(this T obj, JsonSerializerSettings serializerSettings = null)
+        {
+            try
+            {
+                serializerSettings = serializerSettings ?? new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Auto,
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+
+                return JsonConvert.SerializeObject(obj, serializerSettings);
+            }
+            catch (Exception ex)
+            {
+                string errMessage = $"Error in json serialization.Ex:{ex}";
+                return string.Empty;
+            }
+        }
+
+        public static T ToObject<T>(this string json, JsonSerializerSettings serializerSettings = null)
+        {
+            try
+            {
+                serializerSettings = serializerSettings ?? new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Auto,
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+
+                return JsonConvert.DeserializeObject<T>(json,serializerSettings);
+            }
+            catch (Exception ex)
+            {
+                string errMessage = $"Error in json serialization.Ex:{ex}";
+
+                return default(T);
+            }
+        }
+
+    }
+}
